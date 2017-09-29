d8c279c41387ec87c544468e92d14d691baaa285
Flowmar <omar.imam@kineticvortex.com>
2017-09-28 20:23:16 -0500

    Add menu

--- src/main.js
+++ src/main.js
@@ -1,19 +1,39 @@
+// Dependencies
 const electron = require('electron');
+
+const menuTemplate = require('./menu');
 
 const {
     app,
     BrowserWindow,
-    Menu
+    Menu,
+    ipcMain: ipc
 } = electron;
 
 let mainWindow;
 
 app.on('ready', _ => {
-    mainWindow: new BrowserWindow({
+    // Create window
+    mainWindow = new BrowserWindow({
         height: 800,
         width: 1024,
         resizable: true
     });
 
+    // Load html file onto window
     mainWindow.loadURL(`file://${__dirname}/liri.html`);
+
+    // Auto open devTools
+    mainWindow.webContents.openDevTools();
+
+    // When the mainWindow closes, remove the window
+    mainWindow.on('closed', _ => {
+        mainWindow = null;
+    });
+
+    // Build the menu
+    const menuContents = Menu.buildFromTemplate(menuTemplate(mainWindow));
+
+    // Set the menu as the application menu
+    Menu.setApplicationMenu(menuContents)
 })
---
+++ src/menu.js
@@ -0,0 +1,94 @@
+const electron = require('electron');
+
+const {
+    app,
+    Menu
+} = electron;
+
+module.exports = mainWindow => {
+    const name = app.getName();
+
+    const template = [{
+            label: 'Liri',
+            // submenu: [{
+            // label: '',
+            // accelerator: 'Shift+CmdOrCtrl+E',
+            // click: menuItem => {
+            // enabledCycleEffect(menuItem.menu.items);
+            // mainWindow.webContents.send('effect-cycle');
+            // }
+            // },
+            // {
+            // label: 'Vanilla',
+            // type: 'radio',
+            // click: _ => mainWindow.webContents.send('effect-choose')
+            // },
+            // {
+            // label: 'Ascii',
+            // type: 'radio',
+            // click: _ => mainWindow.webContents.send('effect-choose', 'ascii')
+            // },
+            // {
+            // label: 'Daltonize',
+            // type: 'radio',
+            // click: _ => mainWindow.webContents.send('effect-choose', 'daltonize')
+            // },
+            // {
+            // label: 'Hex',
+            // type: 'radio',
+            // click: _ => mainWindow.webContents.send('effect-choose', 'hex')
+            // }
+            // ]
+        }
+        // {
+        //     label: '',
+        //     submenu: [{
+        //         // label: 'Photos Directory',
+        //         // click: _ => images.openDir(images.getPicturesDir(app))
+        //     }]
+        // }
+    ]
+
+    if (process.platform === 'darwin') {
+
+        const name = app.getName();
+
+        template.unshift({
+            label: name,
+            submenu: [{
+                    label: 'About' + name,
+                    role: 'about'
+                },
+                {
+                    type: 'separator',
+                },
+                {
+                    label: 'Hide ' + name,
+                    accelerator: 'Command+H',
+                    role: 'hide'
+                },
+                {
+                    label: 'Hide Others',
+                    accelerator: 'Command+Shift+H',
+                    role: 'hideothers'
+                },
+                {
+                    label: 'Show all',
+                    role: 'unhide'
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    label: 'Quit',
+                    accelerator: 'Command+Q',
+                    click: _ => {
+                        app.quit();
+                    }
+                }
+            ]
+        })
+    }
+
+    return template;
+}
